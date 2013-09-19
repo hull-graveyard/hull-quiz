2694f834a19a7b252b0f1f6852b61af22f638d29
Romain Dardour <romain@hull.io>
2013-09-19 12:23:34 +0200

    Generated at Thu Sep 19 12:23:34 CEST 2013

---
+++ scripts/hull-init.js
@@ -0,0 +1,5 @@
+Hull.init({
+  appId: "512633779e3903afd4000020",
+  orgUrl: 'http://hull-demos.alpha.hullapp.io'
+});
+
---
+++ widgets/background/main.js
@@ -0,0 +1,21 @@
+// This widget listens to all events from the quiz
+// to set the proper background color
+
+/*global Hull:true */
+Hull.widget("background", {
+  templates: [ 'main' ],
+  initialize: function() {
+    var $body = $('body');
+
+    this.sandbox.on('hull.quiz.**', function(data){
+      if(data.current){
+        // Change background color
+        $body.css({
+          backgroundColor: (data.current.question) ? data.current.question.description : ''
+        });
+      }
+    }.bind(this));
+
+  }
+});
+
